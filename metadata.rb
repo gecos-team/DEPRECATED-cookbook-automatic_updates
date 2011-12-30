a168bd18fe4577a718ccddbfc8e0e513939d9f97
David Amian <damian@emergya.com>
2011-12-30 15:05:05 +0100

    Added new attributes

--- metadata.rb
+++ metadata.rb
@@ -124,3 +124,53 @@ attribute 'automatic_updates/on_friday_time',
   :required     => "required",
   :default      => "00:00",
   :recipes      => [ 'automatic_updates::automatic_updates' ]
+
+attribute 'automatic_updates/on_saturday',
+  :display_name => "Automatic updates on Saturday?",
+  :description  => "Set start automatic updates on Saturday",
+  :type         => "string",
+  :choice       => ["on","off"],
+  :required     => "required",
+  :default      => "off",
+  :recipes      => [ 'automatic_updates::automatic_updates' ]
+
+attribute 'automatic_updates/on_saturday_time',
+  :display_name => "Time (HH:MM)",
+  :description  => "Hour to start automatic updates on Saturday (HH:MM)",
+  :type         => "string",
+  :validation   => "custom",
+  :custom       => "^((\d\d:\d\d))$",
+  :required     => "required",
+  :default      => "00:00",
+  :recipes      => [ 'automatic_updates::automatic_updates' ]
+
+attribute 'automatic_updates/on_sunday',
+  :display_name => "Automatic updates on Sunday?",
+  :description  => "Set start automatic updates on Sunday",
+  :type         => "string",
+  :choice       => ["on","off"],
+  :required     => "required",
+  :default      => "off",
+  :recipes      => [ 'automatic_updates::automatic_updates' ]
+
+attribute 'automatic_updates/on_sunday_time',
+  :display_name => "Time (HH:MM)",
+  :description  => "Hour to start automatic updates on Sunday (HH:MM)",
+  :type         => "string",
+  :validation   => "custom",
+  :custom       => "^((\d\d:\d\d))$",
+  :required     => "required",
+  :default      => "00:00",
+  :recipes      => [ 'automatic_updates::automatic_updates' ]
+
+
+attribute 'automatic_updates/max_random_time_span',
+  :display_name => "Max random time span",
+  :description  => "Minutes to increase time in order to avoid concurrent updates",
+  :type         => "string",
+  :validation   => "custom",
+  :custom       => "^(\d\d\d)$",
+  :required     => "required",
+  :default      => "000",
+  :recipes      => [ 'automatic_updates::automatic_updates' ]
+
